bbc704f83b73dc30359e407d487ff16fd44f0734
Avery Smith <averysinclair@avery-smiths-computer.local>
2016-11-10 19:25:52 -0500

    modifying deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,6 +2,7 @@
 
 USER=$1
 HOST=$2
-PIQTIONARY_FOLDER_PATH=$3
+
+#ssh ${USER}@${HOST}
 
 rsync -azP --delete --exclude='bin,config,node_modules,Jenkinsfile,READE.md,restart.txt' ../ ${USER}@${HOST}:${PIQTIONARY_FOLDER_PATH}
