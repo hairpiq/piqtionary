a3020d29b4746435ef174037128e478ba605abb9
Jackson Gabriel <jackson@hairpiq.com>
2016-11-12 12:49:47 -0500

    Initial commits

--- app.js
+++ app.js
@@ -2,98 +2,77 @@
  * Created by jackman79 on 10/31/16.
  */
 
-
-// module.exports = {
-//   getTopRatedTag: function(imagePath) {
     var Clarifai = require("clarifai");
 
-    var http = require('http'),
-        express = require('express'),
-        path = require('path');
+    var config = require('./config/clarifai');
 
     var api = new Clarifai.App(
-        'wONTOYtXMLE9KUXNK5KanWepLay8Etw7tSuuuNt2',
-        '2RENXai2iC2VYPYEuGaZBJ-oodCmrmzD0tfos98D'
+        config.clarifai.client_id,
+        config.clarifai.client_secret
     );
 
+    var vm = this;
 
 
-   var imagePath = "http://www.outfittrends.com/wp-content/uploads/2014/10/cute-black-girls-hairstyles.jpg";
-   var results;
-   api.models.predict(Clarifai.GENERAL_MODEL, imagePath).then(
-     function(response){
-            if(response)
-                results=resultHandler(response);
-                //console.log(response);
+    var imagePath = "http://www.outfittrends.com/wp-content/uploads/2014/10/cute-black-girls-hairstyles.jpg";
+    var results;
 
-         },
-         function (err) {
-          identifyClarifaiError(err);
+    api.models.predict(config.clarifai.model_id, imagePath).then(
+        function (response) {
+            results =  getTopRatedTagHandler(response);
+            //console.log(response);
+            onResults();
+             if (vm.text)
+                console.log(vm.text);
+        },
+        function (err) {
+            identifyClarifaiError(err);
         }
     );
 
-    //console.log(results);
-      // if(results)
-      // console.log(results);
+    if (vm.text)
+        console.log(vm.text);
 
 
-// app.inputs.create({
-//   url: "http://www.outfittrends.com/wp-content/uploads/2014/10/cute-black-girls-hairstyles.jpg"
-// }).then(
-//   function(response) {
-//     console.log(response);
-//   },
-//   function(err) {
-//     //console.log(err);
-//   }
-// );
-//
-// app.inputs.get('f35c4cfca8a44dcaaa7bd75427272cf9').then(
-//   function(response) {
-//     console.log(response._rawData);
-//   },
-//   function(err) {
-//     // there was an error
-//   }
-// );
+    function onResults() {
+        vm.text=results;
+        //console.log(vm.text);
+    }
 
-    var resultHandler = function(response) {
-      var maxValue = 0;
-      var maxRecord = {};
-      response.data.outputs.forEach(function (output) {
+
+    function getTopRatedTagHandler(response) {
+        var maxValue = 0;
+        var maxRecord = {};
+        response.data.outputs.forEach(function (output) {
             //console.log(output.data.concepts);
             output.data.concepts.forEach(function (tag) {
-              tag.value = Math.ceil(tag.value * 100);
-              if (tag.value > maxValue) {
-                maxValue = tag.value;
-                maxRecord = tag;
-              }
+                tag.value = Math.ceil(tag.value * 100);
+                if (tag.value > maxValue) {
+                    maxValue = tag.value;
+                    maxRecord = tag;
+                }
 
             });
 
-      });
-      return maxRecord;
-    };
+        });
+        return maxRecord;
+    }
 
-
-    var identifyClarifaiError = function(err) {
-      if (typeof err["status_code"] === "string" && err["status_code"] === "TIMEOUT") {
-        console.log("TAG request timed out");
-      }
-      else if (typeof err["status_code"] === "string" && err["status_code"] === "ALL_ERROR") {
-        console.log("TAG request received ALL_ERROR. Contact Clarifai support if it continues.");
-      }
-      else if (typeof err["status_code"] === "string" && err["status_code"] === "TOKEN_FAILURE") {
-        console.log("TAG request received TOKEN_FAILURE. Contact Clarifai support if it continues.");
-      }
-      else if (typeof err["status_code"] === "string" && err["status_code"] === "ERROR_THROTTLED") {
-        console.log("Clarifai host is throttling this application.");
-      }
-      else {
-        console.log("TAG request encountered an unexpected error: ");
-        console.log(err);
-      }
-    };
-
-//   }
-// }
+    function identifyClarifaiError(err) {
+        if (typeof err["status_code"] === "string" && err["status_code"] === "TIMEOUT") {
+            console.log("TAG request timed out");
+        }
+        else if (typeof err["status_code"] === "string" && err["status_code"] === "ALL_ERROR") {
+            console.log("TAG request received ALL_ERROR. Contact Clarifai support if it continues.");
+        }
+        else if (typeof err["status_code"] === "string" && err["status_code"] === "TOKEN_FAILURE") {
+            console.log("TAG request received TOKEN_FAILURE. Contact Clarifai support if it continues.");
+        }
+        else if (typeof err["status_code"] === "string" && err["status_code"] === "ERROR_THROTTLED") {
+            console.log("Clarifai host is throttling this application.");
+        }
+        else {
+            console.log("TAG request encountered an unexpected error: ");
+            console.log(err);
+        }
+    }
---
+++ config/clarifai.js
@@ -0,0 +1,7 @@
+module.exports = {
+	clarifai: {
+		client_id: 'yunwntb-DAhRtp9N2AiQ8cWpN9_yKiOH6i4SK_iB',
+		client_secret: 'hIBLeBo-gXLqbMfPM_6w6t4RGgddHkzxSQm3LcQ0',
+		model_id : 'a0ba13cf58eb4d77860e31c135f56501'
+	}
+};
