bb8e7617ccd5cb5188a6eea4ffe0737cb8dfe7d4
Avery Smith <averysinclair@avery-smiths-computer.local>
2016-11-13 10:36:05 -0500

    fixed rsyncignore issues

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,6 +5,6 @@ HOST=$2
 WORKSPACE=$3
 FOLDER_PATH=$4
 
-rsync -azP --delete --exclude-from='bin/rysncignore.txt' ${WORKSPACE}/ ${USER}@${HOST}:${FOLDER_PATH}
+rsync -azP --delete --exclude-from='bin/rsyncignore.txt' ${WORKSPACE}/ ${USER}@${HOST}:${FOLDER_PATH}
 
 ssh ${USER}@${HOST} passenger-config restart-app ${FOLDER_PATH}
