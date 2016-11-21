91d5c546b0b06b1eead05ceca2a278a636a2a914
Avery Smith <avery@hairpiq.com>
2016-11-21 13:30:07 -0500

    updated messaging

--- hairpiq/messages.js
+++ hairpiq/messages.js
@@ -1,6 +1,6 @@
 module.exports = {
 	a: {
-		one: "Hi there!\n\nTo create a Hairpiq, text us a selfie *with* the name of the hairstyle and your IG username in the following format:\n\nStyle Name @username",
+		one: "Hi there!\n\nTo create a hairpiq, text us a selfie *with* the name of the hairstyle and your IG username in the following format:\n\nStyle Name @username",
 		two: "please resend your photo *with* the stylename and your IG username in the following format:\n\nStyle Name @username"
 	},
 	b: {
@@ -8,7 +8,7 @@ module.exports = {
 		two: "your hairpiq.\n\nWould you like it to be featured on hairpiq.com, to include our IG and FB?\n\nreply YES or NO"
 	},
 	c: {
-		one: "Cool! We are excited to review your Hairpiq and if approved, it will be featured on Hairpiq, the world's largest database of hairstyles.",
+		one: "Cool! We are excited to review your hairpiq and if approved, it will be featured on hairpiq.com, the world's largest database of hairstyles.",
 		two: "np. Have fun!",
 		three: "We'll take that non-yes as a no :-). np. Have fun!"
 	}
