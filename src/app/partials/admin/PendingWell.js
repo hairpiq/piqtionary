345c468d8798a9062c0732e227572ce77d5395bb
Avery Smith <averysinclair@avery-smiths-computer.local>
2016-12-17 02:43:17 -0500

    added stub click handlers for buttons on Pending Requests page. Updated layout

--- src/app/partials/admin/PendingWell.js
+++ src/app/partials/admin/PendingWell.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router';
 import InfiniteScroll from 'react-infinite-scroller';
 import Services from '../../services/admin/';
 import PendingItem from './PendingItem';
+import FlatButton from 'material-ui/FlatButton';
 import Dialog from 'material-ui/Dialog';
 
 class PendingWell extends Component {
@@ -15,9 +16,95 @@ class PendingWell extends Component {
       this.state = {
           hairpiqs: [],
           page_num: 0,
-          hasMoreItems: true
+          hasMoreItems: true,
+          dialog: {
+            open: false,
+            title: '',
+            message: '',
+            action: ''
+          }
       };
 
+      this.handleOpen = this.handleOpen.bind(this);
+      this.handleClose = this.handleClose.bind(this);
+      this.handleDialog = this.handleDialog.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+
+  handleOpen = () => {
+    this.setState({dialog: {open: true}});
+  };
+
+  handleClose = () => {
+    this.setState({dialog: {open: false}});
+  };
+
+  handleDialog(obj) {
+    
+    console.log(obj);
+
+    var title;
+    var message;
+
+    switch (obj.action) {
+      case 'REJECT':
+
+        title = 'REJECT HAIRPIQ';
+        message = 'Are you sure you want to reject this hairpiq? Doing so will send it to the Trash Section.';
+
+        break;
+
+      case 'UPDATE':
+
+        title = 'UPDATE HAIRPIQ';
+        message = 'Are you sure you want to update this hairpiq? This will allow you to modify the text in the image.';
+
+        break;
+
+      case 'APPROVE':
+
+        title = 'APPROVE HAIRPIQ';
+        message = 'Are you sure you want to approve this hairpiq? Doing so will send it to the Unpublished Section.';
+
+        break;
+    }
+
+    this.setState({
+      dialog: {
+        open: true,
+        title: title,
+        message: message,
+        action: obj.action
+      }
+    });
+  }
+
+  handleSubmit() {
+
+    const action = this.state.dialog.action;
+    
+    switch (action) {
+      case 'REJECT':
+
+        console.log('rejection submitted');
+
+        break;
+
+      case 'UPDATE':
+
+        console.log('update submitted');
+
+        break;
+
+      case 'APPROVE':
+
+        console.log('approval submitted');
+
+        break;
+    }
+
+    this.handleClose();
   }
 
   loadItems(page) {
@@ -64,17 +151,34 @@ class PendingWell extends Component {
       this.state.hairpiqs.map((listItem, i) => {
         items.push(
             
-            <div className="uk-width-medium-6-10 uk-push-2-10">
+            <div className="uk-width-medium-1-2">
               <PendingItem
                 key={i}
                 listItem={listItem}
                 location={this.props.location}
                 hairpiqs={this.state.hairpiqs}
+                handleDialog={this.handleDialog}
               />
             </div>
 
         );
     });
+
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={this.handleClose}
+        onClick={this.handleClose}
+      />,
+      <FlatButton
+        label="Submit"
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={this.handleSubmit}
+        onClick={this.handleSubmit}
+      />,
+    ];
 
     return (
       
@@ -93,6 +197,17 @@ class PendingWell extends Component {
               </div>
           </InfiniteScroll>
         
+        </div>
+
+        <div>
+          <Dialog
+            title={this.state.dialog.title}
+            actions={actions}
+            modal={false}
+            open={this.state.dialog.open}
+            onRequestClose={this.handleClose}>
+            {this.state.dialog.message}
+          </Dialog>
         </div>
 
       </div>
