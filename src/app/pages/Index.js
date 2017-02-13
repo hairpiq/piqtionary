9eb170d4e95f57c0dedcdd8dcad1109f044bacc4
Avery Smith <averysinclair@avery-smiths-computer.local>
2017-02-13 11:39:36 -0500

    updated splash graphics for production

--- src/app/pages/Index.js
+++ src/app/pages/Index.js
@@ -70,6 +70,43 @@ class Index extends Component {
 
     )
 
+    const strip_entries = (
+
+      <div>
+
+        <SplashItem
+          listItem={{
+            ig_username: "@naturallynefertiti",
+            s3_url: "https://piqtionary.s3.amazonaws.com/ycda4hhudghrbokwujud.jpg",
+            stylename: "Afro",
+          }}
+        />
+        <SplashItem
+          listItem={{
+            ig_username: "@napturally_tamed",
+            s3_url: "https://piqtionary.s3.amazonaws.com/klnbxswlzglzpdzirudz.jpg",
+            stylename: "Perm Rod Set Updo",
+          }}
+        />
+        <SplashItem
+          listItem={{
+            ig_username: "@everythingmich",
+            s3_url: "https://piqtionary.s3.amazonaws.com/woshamxizv3b7avxgs2g.jpg",
+            stylename: "Side Updo",
+          }}
+        />
+        <SplashItem
+          listItem={{
+            ig_username: "@curlyyhair.killa",
+            s3_url: "https://piqtionary.s3.amazonaws.com/tc286wiqkorb9pjqo93r.jpg",
+            stylename: "Twist Bangs and Puff",
+          }}
+        />
+
+      </div>
+
+    )
+
     return (
       <div>
 
@@ -91,78 +128,9 @@ class Index extends Component {
 
           <div className="left-col">
             <div className="strip">
-              <SplashItem
-                listItem={{
-                  _id: "5884ffd28516685c6087d544",
-                  ig_username: "@curlyyhair.killa",
-                  publish_status: "published",
-                  s3_url: "https://dev-piqtionary.s3.amazonaws.com/exbclo4ytos9zbg8yttz.jpg",
-                  stylename: "Twist Bangs and Puff",
-                }}
-              />
-              <SplashItem
-                listItem={{
-                  _id: "5884ffd28516685c6087d544",
-                  ig_username: "@curlyyhair.killa",
-                  publish_status: "published",
-                  s3_url: "https://dev-piqtionary.s3.amazonaws.com/v4drwif6adude07bobmt.jpg",
-                  stylename: "Twist Bangs and Puff",
-                }}
-              />
-              <SplashItem
-                listItem={{
-                  _id: "5884ffd28516685c6087d544",
-                  ig_username: "@curlyyhair.killa",
-                  publish_status: "published",
-                  s3_url: "https://dev-piqtionary.s3.amazonaws.com/llhsufy0edxlam7d68km.jpg",
-                  stylename: "Twist Bangs and Puff",
-                }}
-              />
-              <SplashItem
-                listItem={{
-                  _id: "5884ffd28516685c6087d544",
-                  ig_username: "@curlyyhair.killa",
-                  publish_status: "published",
-                  s3_url: "https://dev-piqtionary.s3.amazonaws.com/qm8ssoz2jjk1yl4uyhvp.jpg",
-                  stylename: "Twist Bangs and Puff",
-                }}
-              />
-              <SplashItem
-                listItem={{
-                  _id: "5884ffd28516685c6087d544",
-                  ig_username: "@curlyyhair.killa",
-                  publish_status: "published",
-                  s3_url: "https://dev-piqtionary.s3.amazonaws.com/exbclo4ytos9zbg8yttz.jpg",
-                  stylename: "Twist Bangs and Puff",
-                }}
-              />
-              <SplashItem
-                listItem={{
-                  _id: "5884ffd28516685c6087d544",
-                  ig_username: "@curlyyhair.killa",
-                  publish_status: "published",
-                  s3_url: "https://dev-piqtionary.s3.amazonaws.com/v4drwif6adude07bobmt.jpg",
-                  stylename: "Twist Bangs and Puff",
-                }}
-              />
-              <SplashItem
-                listItem={{
-                  _id: "5884ffd28516685c6087d544",
-                  ig_username: "@curlyyhair.killa",
-                  publish_status: "published",
-                  s3_url: "https://dev-piqtionary.s3.amazonaws.com/llhsufy0edxlam7d68km.jpg",
-                  stylename: "Twist Bangs and Puff",
-                }}
-              />
-              <SplashItem
-                listItem={{
-                  _id: "5884ffd28516685c6087d544",
-                  ig_username: "@curlyyhair.killa",
-                  publish_status: "published",
-                  s3_url: "https://dev-piqtionary.s3.amazonaws.com/qm8ssoz2jjk1yl4uyhvp.jpg",
-                  stylename: "Twist Bangs and Puff",
-                }}
-              />
+              {strip_entries}
+              { /*repeat strip entries for smooth looping animation */}
+              {strip_entries}
             </div>
           </div>
 
